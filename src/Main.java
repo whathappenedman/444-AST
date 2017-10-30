f565881e6c93d060ff2fd9be0cb7669e10028096
Unknown <whathappenedman@gmail.com>
2017-10-29 19:10:08 -0700

    SAVE WORK

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello world");
+		String test = "kwdprog { 7 }";
+		Scanner kb = new Scanner(test);
+		String x = "";
+		
+		ParseTable pt = new ParseTable();
+		while (kb.hasNext()){
+			x = kb.next();
+			if (x.equals("-1")){
+				break;
+			}
+			System.out.println(x);
+		}
 	}
 
 }
---
+++ src/ParseTable.java
@@ -0,0 +1,7 @@
+
+public class ParseTable {
+	public String[][] pt = new String[27][50];
+	ParseTable(){
+		pt[0][0] = "";
+	}
+}
