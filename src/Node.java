29112dc187814fca13573e19b0b7bd5a322e4010
MasahiroMarkTsujimura <masahiromarktsujimura@gmail.com>
2017-10-29 21:27:19 -0700

    changed implementation of childrens

--- src/Node.java
+++ src/Node.java
@@ -1,45 +1,40 @@
+import java.util.*;
+
 public class Node {
 
-	public Node parentNode;
-	public Node leftChildNode;
-	public Node rightChildNode;
+	public Node parentNode = null;
+	ArrayList children = new ArrayList();
 	public int id;
 	public String name;
 	public String value;
 		
 	public Node (Node ParentNode)
 	{
-		this.parentNode = ParentNode	
+		this.parentNode = ParentNode;
 	}
 	public Node (Node ParentNode, String nodeName)
 	{
 		this.parentNode = ParentNode;
 		this.name = nodeName;	
 	}
-	public Node (Node ParentNode, String nodeName, String nodeValue)
+	public Node (Node ParentNode, String nodeName, String nodeValue, int nodeId)
 	{
 		this.parentNode = ParentNode;
 		this.name = nodeName;
-		this.value = nodeValue;		
+		this.value = nodeValue;	
+		this.id = nodeId;
 	}	
-	public void setLeftChild(Node node)
+	public void addChild(Node node)
 	{
-		this.leftChildNode = node;
-	}
-	public void setRightChild(Node node)
-	{
-		this.rightChildNode = node;	
+		this.children.add(node);
 	}
 	public Node getParent()
 	{
 		return parentNode;
 	}
-	public Node getLeftChild()
+	public Node getChild(int num)
 	{
-		return leftChildNode;
+		return children.get(num);
 	}
-	public Node getRightChild()
-	{
-		return rightChildNode;	
-	}
+
 }
