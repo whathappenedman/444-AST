4d9a9f6d7d22d58f3bc6d94ac220f0cba574ac87
MasahiroMarkTsujimura <masahiromarktsujimura@gmail.com>
2017-10-29 21:39:55 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,11 @@ import java.util.Scanner;
 
 public class Main {
 
+
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		String test = "kwdprog { 7 }";
 		Scanner kb = new Scanner(test);
 		String x = "";
@@ -16,6 +19,101 @@ public class Main {
 			}
 			System.out.println(x);
 		}
+		
+		
+		//terminal tokens
+		String current_token = "";
+		current_token = scan.next();
+		while (kb.hasNext()) {
+			switch (current_token) {
+				case "kwdprog":
+					token_number = -1;
+					break;
+				case "kwdvars":
+					token_number = -2;
+					break;
+				case "kprint":
+					token_number = -3;
+					break;
+				case "kwdwhile":
+					token_number = -4;
+					break;
+				case "int":
+					token_number = -5;
+					break;
+				case "float":
+					token_number = -6;
+					break;
+				case "string":
+					token_number = -7;
+					break;
+				case "id":
+					token_number = -8;
+					break;
+				case "(":
+					token_number = -9;
+					break;	
+				case ")":
+					token_number = -10;
+					break;	
+				case "{":
+					token_number = -11;
+					break;	
+				case "}":
+					token_number = -12;
+					break;	
+				case ";":
+					token_number = -13;
+					break;	
+				case ",":
+					token_number = -14;
+					break;	
+				case "=":
+					token_number = -15;
+					break;	
+				case "==":
+					token_number = -16;
+					break;	
+				case "!=":
+					token_number = -17;
+					break;	
+				case "<":
+					token_number = -18;
+					break;	
+				case "<=":
+					token_number = -19;
+					break;	
+				case ">=":
+					token_number = -20;
+					break;	
+				case ">":
+					token_number = -21;
+					break;	
+				case "+":
+					token_number = -22;
+					break;
+				case "-":
+					token_number = -23;
+					break;
+				case "*":
+					token_number = -24;
+					break;
+				case "/":
+					token_number = -25;
+					break;
+				case "^":
+					token_number = -26;
+					break;
+				default:
+					token_number = -27;
+					break;
+			}
+		
+		
+			
+		
+		
+		
 	}
 
 }
