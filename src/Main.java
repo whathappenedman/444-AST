c2913a0a174bb19d8e41b0aef04d05af040ee740
Mark <masahiromarktsujimura@gmail.com>
2017-10-31 14:08:12 -0700

    saved work

--- src/Main.java
+++ src/Main.java
@@ -7,30 +7,29 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String test = "kwdprog { 7 }";
+		String test = "kwdprog { int }";
 		Scanner kb = new Scanner(test);
+
+		/*
 		String x = "";
-		
-//		ParseTable pt = new ParseTable();
-//		while (kb.hasNext()){
-//			x = kb.next();
-//			if (x.equals("-1")){
-//				break;
-//			}
-//			System.out.println(x);
-		
+		ParseTable pt = new ParseTable();
+		while (kb.hasNext()){
+			x = kb.next();
+			if (x.equals("-1")){
+				break;
+			}
+			System.out.println(x);
+		*/
 		
 		int token_number;
-		//terminal tokens
 		String current_token = "";
-		//current_token = kb.next();
+		//current_token = kb.next();			//Add after testing
 		while (kb.hasNext()) {
-			current_token = kb.next();			//testing 
+			current_token = kb.next();			//testing. Remove after
 			switch (current_token) {
 				case "kwdprog":
 					token_number = -1;
-					System.out.println("kwdprog");
-					
+					//System.out.println("kwdprog");
 					break;
 				case "kwdvars":
 					token_number = -2;
@@ -41,7 +40,7 @@ public class Main {
 				case "kwdwhile":
 					token_number = -4;
 					break;
-				case "int":
+				case "int":	
 					token_number = -5;
 					break;
 				case "float":
@@ -61,11 +60,11 @@ public class Main {
 					break;	
 				case "{":
 					token_number = -11;
-					System.out.println("{");
+					//System.out.println("{");
 					break;	
 				case "}":
 					token_number = -12;
-					System.out.println("}");
+					//System.out.println("}");
 					break;	
 				case ";":
 					token_number = -13;
@@ -111,27 +110,69 @@ public class Main {
 					break;
 				default:
 					token_number = -27;
-					System.out.println("non terminal");
+					//System.out.println("non terminal");
 					break;
 			}
 		
-	/*	
-		//To Do
-		//get the value up from parsetable
-		Stack<Integer> s = new Stack<>();
-		s.pop(1);	//so it runs the first time
+		//Stack and it's manipulation
+		Stack<String> s = new Stack<>();
+		s.push("Pgm");	//so it runs the first time
 		stacktop = s.peek();
 		if (stacktop > 0) {
 			table_entry = ParseTable(stacktop, abs(token_number));	
 			switch (table_entry) {
-				case 1:	
-					//To DO
-					//Pop and push for it
-					break;		
-				//To DO
-				// Rest of the cases
-		*/
-		
+				case 1:	//Pgm = kwdprog BBlock
+					//TODO create Tree Node
+					
+					s.pop();
+					s.push("BBlock");
+					s.push("kwdprog");
+					break;
+				case 2: //BBlock = brace1 Vargroup Stmts brace2
+					s.pop();
+					s.push("}");
+					s.push("Stmts");
+					s.push("Vargroup");
+					s.push("{");
+					break;
+				case 3: //Vargroup = kwdvars PPvarlist
+					s.pop();
+					s.push("PPvarlist");
+					s.push("kwdvars");
+					break;
+				case 4:	//Vargroup = eps
+					break;
+				case 5: //PPvarlist = parens1 Varlist parens2
+					s.pop();
+					s.push(")");
+					s.push("Varlist");
+					s.push("(");
+					break;
+				case 6: //Varlist = Vardecl semi Varlist
+					s.pop();
+					s.push("Varlist");
+					s.push(";");
+					break;
+				case 7: //Varlist = eps
+					break;
+				case 8: //Vardecl = Basekind Varid
+					s.pop();
+					s.push("Varid");
+					s.push("Basekind");
+					break;
+				case 9: //Basekind = int
+					s.pop()
+					break;
+				case 10: //Basekind = float
+					s.pop()
+					break;
+				case 11: //Basekind = string
+					s.pop()
+					break;
+				case 12: //Varid = id
+					s.pop()
+					break;
+
 		}
 	}	
 }
